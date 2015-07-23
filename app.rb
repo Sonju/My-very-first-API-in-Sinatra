7342501946cb768be0c824646c588708fda6f964
Sonju Walker <sonjuwalker@gmail.com>
2015-07-23 14:33:12 -0500

    add views

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+# require bundler && gems from Gemfile
 require 'bundler'
 Bundler.require()
 
@@ -9,4 +10,12 @@ end
 # http://locations/api/awesome
 get '/api/awesome' do
   {:name => 'Cool Server', :message => 'Its cool to build servers!'}.to_json
+end
+
+get '/' do
+  erb :index    #erb says go find 'index.erb' file and share it.
+end
+
+get '/api/json' do  # get: localhost/api/json
+  some_json
 end
