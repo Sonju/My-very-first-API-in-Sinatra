0e3db22778ee610e7bca520df5dc9fd66d72f937
Sonju Walker <sonjuwalker@gmail.com>
2015-07-23 14:50:16 -0500

    up

--- app.rb
+++ app.rb
@@ -3,9 +3,9 @@ require 'bundler'
 Bundler.require()
 
 # http://localhost/
-get '/' do
-  {:name => 'Darth Vader', :message => 'The force is strong with this one'}.to_json
-end
+# get '/' do
+#   {:name => 'Darth Vader', :message => 'The force is strong with this one'}.to_json
+# end
 
 # http://locations/api/awesome
 get '/api/awesome' do
